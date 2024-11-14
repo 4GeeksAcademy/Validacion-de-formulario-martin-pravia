2b84b4792dacca921c1f222abd4b98b417ef04e4
Martín Pravia <marto.pravia@gmail.com>
2024-11-14 15:07:13 +0000

    defino constantes de todos los id de cada campo a traves de doc.qrySct

--- src/app.js
+++ src/app.js
@@ -8,5 +8,13 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   document.getElementById("sendForm").addEventListener("click", function(e) {
     e.preventDefault();
+    const cardNumber = document.querySelector("#cardNumber");
+    const cVC = document.querySelector("#CVC");
+    const amount = document.getElementById("amount");
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const city = document.getElementById("city");
+    const state = document.getElementById("state");
+    const pCode = document.getElementById("pCode");
   });
 };

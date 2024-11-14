301de46cdc398ee51365e98b1533b093f5d1da82
Martín Pravia <marto.pravia@gmail.com>
2024-11-14 15:03:06 +0000

    Creo la funcion para detener el flujo de envio con preventDefault();

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("sendForm").addEventListener("click", function(e) {
+    e.preventDefault();
+  });
 };

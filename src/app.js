b3265b5389db193819bdea2668b11429e56cf866
Martín Pravia <marto.pravia@gmail.com>
2024-11-15 12:42:18 +0000

    validacion con mensajes ok, cambio a verde y blanco cuando queda correctamente completado

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   document.getElementById("sendForm").addEventListener("click", function(e) {
     e.preventDefault();
+
     const cardNumber = document.querySelector("#cardNumber");
     const cVC = document.querySelector("#CVC");
     const amount = document.getElementById("amount");
@@ -18,69 +19,66 @@ window.onload = function() {
     const postalCode = document.getElementById("pCode");
 
     const alertMessage = document.querySelector(".alert");
-    alertMessage.style.display = "none";
-    alertMessage.style.textContent = "";
+    alertMessage.classList.add("d-none");
+    alertMessage.innerHTML = "";
 
-    const inputs = [
-      cardNumber,
-      cVC,
-      amount,
-      firstName,
-      lastName,
-      city,
-      state,
-      postalCode
-    ];
-    for (const input of inputs) {
-      input.style.backgroundColor = "";
+    let campoValido = true;
+    const error = [];
 
-      let campoValido = true;
-      const error = [];
+    if (cardNumber.value.length !== 16 || isNaN(cardNumber.value)) {
+      error.push("La tarjeta debe de tener 16 digitos. <br/>");
+      cardNumber.style.backgroundColor = "red";
+      campoValido = false;
+    }
 
-      if (cardNumber.value.length !== 16 || isNaN(cardNumber.value)) {
-        error.push("La tarjeta debe de tener 16 digitos");
-        cardNumber.style.backgroundColor = "red";
-        campoValido = false;
-      }
-
-      if (
-        (cVC.value.length !== 3 && cVC.value.length !== 4) ||
-        isNaN(cVC.value)
-      ) {
-        error.push("CVC must be 3 or 4 digits.");
-        cVC.style.backgroundColor = "red";
-        campoValido = false;
-      }
-      if (isNaN(amount.value) || amount.value <= 0) {
-        error.push("El importe debe de ser mayor a $0");
-        amount.style.backgroundColor = "red";
-        campoValido = false;
-      }
-      if (!firstName.value) {
-        error.push("Complete su nmombre por favor");
-        firstName.style.backgroundColor = "red";
-        campoValido = false;
-      }
-      if (!lastName.value) {
-        error.push("Complete su apellido por favor");
-        lastName.style.backgroundColor = "red";
-        campoValido = false;
-      }
-      if (!city.value) {
-        error.push("Complete su ciudad por favor");
-        city.style.backgroundColor = "red";
-        campoValido = false;
-      }
-      if (state.value === "Pick a state") {
-        error.push("Seleccione un estado por favor");
-        state.style.backgroundColor = "red";
-        campoValido = false;
-      }
-      if (postalCode.value.length !== 5 || isNaN(postalCode.value)) {
-        error.push("Su codigo postal debe de ser de 5 dígitos, verifiquelo.");
-        postalCode.style.backgroundColor = "red";
-        campoValido = false;
-      }
+    if (
+      (cVC.value.length !== 3 && cVC.value.length !== 4) ||
+      isNaN(cVC.value)
+    ) {
+      error.push("El CVC debe de tener 3 o 4 dígitos. <br/>");
+      cVC.style.backgroundColor = "red";
+      campoValido = false;
+    }
+    if (isNaN(amount.value) || amount.value <= 0) {
+      error.push("El importe debe de ser mayor a $0. <br/>");
+      amount.style.backgroundColor = "red";
+      campoValido = false;
+    }
+    if (!firstName.value) {
+      error.push("Complete su nombre por favor. <br/>");
+      firstName.style.backgroundColor = "red";
+      campoValido = false;
+    }
+    if (!lastName.value) {
+      error.push("Complete su apellido por favor. <br/>");
+      lastName.style.backgroundColor = "red";
+      campoValido = false;
+    }
+    if (!city.value) {
+      error.push("Complete su ciudad por favor. <br/>");
+      city.style.backgroundColor = "red";
+      campoValido = false;
+    }
+    if (state.value === "Pick a state") {
+      error.push("Seleccione un estado por favor. <br/>");
+      state.style.backgroundColor = "red";
+      campoValido = false;
+    }
+    if (postalCode.value.length !== 5 || isNaN(postalCode.value)) {
+      error.push(
+        "Su codigo postal debe de ser de 5 dígitos, verifiquelo.<br/>"
+      );
+      postalCode.style.backgroundColor = "red";
+      campoValido = false;
+    }
+    if (campoValido === false) {
+      alertMessage.classList.remove("d-none");
+      alertMessage.innerHTML = error.join(" ");
+    } else {
+      alertMessage.classList.remove("d-none", "text-danger");
+      alertMessage.style.backgroundColor = "green";
+      alertMessage.style.color = "white";
+      alertMessage.innerHTML = "Formulario completado correctamente";
     }
   });
 };
